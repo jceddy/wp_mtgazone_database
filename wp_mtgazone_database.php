0958d59e57675de858f6159b31ae987fd96ae38b
jceddy <jceddy@gmail.com>
2019-09-24 10:33:33 -0500

    Update wp_mtgazone_database.php

--- wp_mtgazone_database.php
+++ wp_mtgazone_database.php
@@ -3,10 +3,21 @@
  * Plugin Name: MTGAZone Card Database
  * Plugin URI: https://github.com/jceddy/wp_mtgazone_database
  * Description: Plugin for populating the MTGAZone Card Database.
- * Version: 1.0
+ * Version: 0.1
  * Author: Joseph Eddy
  * Author URI: https://www.patreon.com/DailyArena
  */
+
+add_filter('cron_schedules', 'wmd_add_cron_interval');
+
+function wmd_add_cron_interval($schedules) {
+  $schedules['five_seconds'] = array(
+    'interval' => 5,
+    'display' => esc_html__('Every Five Seconds');
+  );
+  
+  return $schedules;
+}
 
 add_action('wmd_import_cards_hook', 'wmd_import_cards_exec');
 
@@ -19,4 +30,8 @@ register_deactivation_hook(__FILE__, 'wmd_deactivate');
 function wmd_deactivate() {
   $timestamp = wp_next_scheduled('wmd_import_cards_hook');
   wp_unschedule_event($timestamp, 'wmd_import_cards_hook');
+}
+
+function wmd_import_cards_exec() {
+  // code to import cards here 
 }
