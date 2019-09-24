6459de4aed93806e4545fdfa5365bb9a0e9830e7
jceddy <jceddy@gmail.com>
2019-09-24 10:29:48 -0500

    Update wp_mtgazone_database.php

--- wp_mtgazone_database.php
+++ wp_mtgazone_database.php
@@ -2,8 +2,21 @@
 /**
  * Plugin Name: MTGAZone Card Database
  * Plugin URI: https://github.com/jceddy/wp_mtgazone_database
- * Description: PLugin for populating the MTGAZone Card Database.
+ * Description: Plugin for populating the MTGAZone Card Database.
  * Version: 1.0
  * Author: Joseph Eddy
  * Author URI: https://www.patreon.com/DailyArena
  */
+
+add_action('wmd_import_cards_hook', 'wmd_import_cards_exec');
+
+if(!wp_next_scheduled('wmd_import_cards_hook')) {
+  wp_schedule_event(time(), 'five_seconds', 'wmd_import_cards_hook'); 
+}
+
+register_deactivation_hook(__FILE__, 'wmd_deactivate');
+
+function wmd_deactivate() {
+  $timestamp = wp_next_scheduled('wmd_import_cards_hook');
+  wp_unschedule_event($timestamp, 'wmd_import_cards_hook');
+}
