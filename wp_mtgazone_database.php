b8fcaa922844b9640c19d155b5c3bb3fc883545e
jceddy <jceddy@gmail.com>
2019-09-24 10:11:55 -0500

    Create wp_mtgazone_database.php

---
+++ wp_mtgazone_database.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: MTGAZone Card Database
+ * Plugin URI: https://github.com/jceddy/wp_mtgazone_database
+ * Description: PLugin for populating the MTGAZone Card Database.
+ * Version: 1.0
+ * Author: Joseph Eddy
+ * Author URI: https://www.patreon.com/DailyArena
+ */
